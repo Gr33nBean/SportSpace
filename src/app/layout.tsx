3da7fe98f19bbc181ccf1982dd7a69a6b7aca494
Gr33nBean <20520206@gm.uit.edu.vn>
2024-06-28 23:51:48 +0700

    chore: language in html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='vi'>
       <head>
         <title>SportSpace</title>
       </head>
