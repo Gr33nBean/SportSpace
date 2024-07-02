2f02597f7cc67dca8587511f79b3e651bfbbf8c5
Gr33nBean <20520206@gm.uit.edu.vn>
2024-07-02 23:12:23 +0700

    feat: ui header, footer

--- src/components/Common/Button/PrimaryButton.tsx
+++ src/components/Common/Button/PrimaryButton.tsx
@@ -11,7 +11,7 @@ const PrimaryButton = ({
 }) => {
   return (
     <button
-      className={`duration-50 rounded-md border-secondary px-2 py-1 text-xs font-semibold transition-all hover:opacity-90 active:shadow-lg ${variant === 'filled' ? 'bg-secondary text-white' : 'border text-secondary'}`}
+      className={`duration-50 rounded-md border-secondary px-2 py-2 text-xs font-semibold transition-all hover:opacity-90 active:shadow-lg md:py-1 ${variant === 'filled' ? 'bg-secondary text-white' : 'border text-secondary'} `}
       {...props}
     >
       {children}
--- src/components/Layout/MainHeader/index.tsx
+++ src/components/Layout/MainHeader/index.tsx
@@ -22,28 +22,73 @@ const pages = [
 ]
 
 const MainHeader = () => {
-  const path = usePathname()
-
+  const [expand, setExpand] = React.useState(false)
   return (
-    <div className='container flex w-full items-center py-4 shadow-sm'>
-      <div className='flex flex-1 items-center gap-6'>
-        <div className='size-8 rounded-full bg-primary'></div>
-        {pages.map((page) => (
-          <Link
-            key={page.name}
-            href={page.href}
-            className={`text-sm font-medium text-primary transition-all duration-150 hover:underline ${'/' + path.split('/')[1] === page.href ? '!font-bold' : ''}`}
-          >
-            {page.name}
-          </Link>
-        ))}
+    <>
+      <div className='container sticky top-0 z-50 w-full bg-white py-4 shadow-sm'>
+        <div className='flex w-full items-center'>
+          <div className='flex flex-1 items-center gap-6'>
+            <div className='size-8 rounded-full bg-primary'></div>
+            <div className='hidden flex-1 items-center gap-6 md:flex'>
+              <ListLink />
+            </div>
+          </div>
+
+          <button className='text-primary md:hidden' onClick={() => setExpand(!expand)}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <line x1='4' x2='20' y1='12' y2='12' />
+              <line x1='4' x2='20' y1='6' y2='6' />
+              <line x1='4' x2='20' y1='18' y2='18' />
+            </svg>
+          </button>
+
+          <div className='hidden items-center gap-2 md:flex'>
+            <PrimaryButton variant='outlined'>Đăng nhập</PrimaryButton>
+            <PrimaryButton>Đăng ký</PrimaryButton>
+          </div>
+        </div>
       </div>
-      <div className='flex items-center gap-2'>
+
+      <div
+        className='container fixed flex w-full flex-col gap-2 bg-white py-4 pt-[88px] shadow-sm transition-all duration-500 md:hidden'
+        style={{
+          top: expand ? '0px' : '-100%',
+          pointerEvents: expand ? 'all' : 'none',
+        }}
+      >
+        <ListLink />
         <PrimaryButton variant='outlined'>Đăng nhập</PrimaryButton>
         <PrimaryButton>Đăng ký</PrimaryButton>
       </div>
-    </div>
+    </>
   )
 }
 
 export default MainHeader
+
+function ListLink() {
+  const path = usePathname()
+  return (
+    <>
+      {pages.map((page) => (
+        <Link
+          key={page.name}
+          href={page.href}
+          className={`block w-full text-center text-sm font-medium text-primary transition-all duration-150 hover:underline md:w-fit ${'/' + path.split('/')[1] === page.href ? '!font-bold' : ''}`}
+        >
+          {page.name}
+        </Link>
+      ))}
+    </>
+  )
+}
