c18c9e528e322323fbaaef90768a4adae300275e
viet.dang <viet.dang@kegmil.com>
2024-07-03 15:14:03 +0700

    feat: handle add create contact us

--- src/components/AboutUs/ContactUs/index.tsx
+++ src/components/AboutUs/ContactUs/index.tsx
@@ -13,11 +13,11 @@ const ContactUs = () => {
         src={images.BACKGROUND_IMG}
         width={600}
         height={100}
-        className='h-[460px] w-full object-cover'
+        className='h-[500px] w-full object-cover'
         alt='about-us'
       />
-      <div className='absolute left-32 top-[20%] w-[45%] bg-white bg-opacity-50'>
-        <div className='flex h-64 flex-col gap-7 p-9'>
+      <div className='absolute left-[20px] top-[20%] w-[90%] bg-white bg-opacity-50 lg:left-32 lg:w-[45%]'>
+        <div className='flex flex-col gap-7 p-9 lg:h-64'>
           <div className='flex flex-col font-Inter text-2xl font-semibold text-secondary'>
             <span>{message.SHARE_VALUE}</span>
             <span>{message.BE_A_PART}</span>
