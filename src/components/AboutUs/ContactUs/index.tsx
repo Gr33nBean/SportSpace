685517e4326f931a2f7247299b2da494f0ff8eef
Gr33nBean <20520206@gm.uit.edu.vn>
2024-07-03 22:34:23 +0700

    fix: header icon, close behavior

--- src/components/AboutUs/ContactUs/index.tsx
+++ src/components/AboutUs/ContactUs/index.tsx
@@ -1,32 +1,31 @@
-import Image from 'next/image'
-
 import React from 'react'
 
 import PrimaryButton from '@/components/Common/Button/PrimaryButton'
 
-import { images, message } from './constants'
+import { message } from './constants'
 
 const ContactUs = () => {
   return (
-    <section className='relative'>
-      <Image
-        src={images.BACKGROUND_IMG}
-        width={600}
-        height={100}
-        className='h-[500px] w-full object-cover'
-        alt='about-us'
-      />
-      <div className='absolute left-[20px] top-[20%] w-[90%] bg-white bg-opacity-50 lg:left-32 lg:w-[45%]'>
-        <div className='flex flex-col gap-7 p-9 lg:h-64'>
-          <div className='flex flex-col font-Inter text-2xl font-semibold text-secondary'>
-            <span>{message.SHARE_VALUE}</span>
-            <span>{message.BE_A_PART}</span>
-          </div>
-          <span className='text-primary'>{message.CONTACT_US_TEXT}</span>
-          <div>
-            <PrimaryButton className='h-10 w-32'>
-              <span className='font-Inter text-base font-bold'>{message.CONTACT_US_BTN}</span>
-            </PrimaryButton>
+    <section className='background-contact'>
+      <div className='container relative h-[60vh] w-full'>
+        <div
+          className='absolute left-1/2 top-1/2 w-[90%] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white bg-opacity-80 lg:left-20 lg:w-[50%] lg:translate-x-0'
+          style={{
+            backdropFilter: 'blur(20px)',
+            WebkitBackdropFilter: 'blur(20px)',
+          }}
+        >
+          <div className='flex flex-col gap-7 p-9'>
+            <div className='flex flex-col font-Inter text-2xl font-semibold text-secondary'>
+              <span>{message.SHARE_VALUE}</span>
+              <span>{message.BE_A_PART}</span>
+            </div>
+            <span className='text-primary'>{message.CONTACT_US_TEXT}</span>
+            <div>
+              <PrimaryButton className='h-10 w-32'>
+                <span className='font-Inter text-base font-bold'>{message.CONTACT_US_BTN}</span>
+              </PrimaryButton>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Layout/MainHeader/index.tsx
+++ src/components/Layout/MainHeader/index.tsx
@@ -6,6 +6,8 @@ import React from 'react'
 
 import PrimaryButton from '@/components/Common/Button/PrimaryButton'
 
+import './style.css'
+
 const pages = [
   {
     name: 'Trang chủ',
@@ -25,34 +27,25 @@ const MainHeader = () => {
   const [expand, setExpand] = React.useState(false)
   return (
     <>
-      <div className='container sticky top-0 z-50 w-full bg-white py-4 shadow-sm'>
+      <div className='container sticky top-0 z-[999] w-full bg-white py-4 shadow-sm'>
         <div className='flex w-full items-center'>
           <div className='flex flex-1 items-center gap-6'>
             <div className='size-8 rounded-full bg-primary'></div>
-            <div className='hidden flex-1 items-center gap-6 md:flex'>
+            <div className='hidden flex-1 items-center gap-6 md:!flex'>
               <ListLink />
             </div>
           </div>
 
-          <button className='text-primary md:hidden' onClick={() => setExpand(!expand)}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            >
-              <line x1='4' x2='20' y1='12' y2='12' />
-              <line x1='4' x2='20' y1='6' y2='6' />
-              <line x1='4' x2='20' y1='18' y2='18' />
-            </svg>
-          </button>
+          <input
+            type='checkbox'
+            role='button'
+            checked={expand}
+            onClick={() => setExpand(!expand)}
+            aria-label='Display the menu'
+            className='menu md:hidden'
+          />
 
-          <div className='hidden items-center gap-2 md:flex'>
+          <div className='hidden items-center gap-2 md:!flex'>
             <PrimaryButton variant='outlined'>Đăng nhập</PrimaryButton>
             <PrimaryButton>Đăng ký</PrimaryButton>
           </div>
@@ -60,15 +53,23 @@ const MainHeader = () => {
       </div>
 
       <div
-        className='container fixed flex w-full flex-col gap-2 bg-white py-4 pt-[88px] shadow-sm transition-all duration-500 md:hidden'
+        className='fixed bottom-0 left-0 right-0 top-0 z-[100] flex items-start pt-[76px] transition-all duration-500 md:hidden'
         style={{
           top: expand ? '0px' : '-100%',
           pointerEvents: expand ? 'all' : 'none',
         }}
+        onClick={() => setExpand(false)}
       >
-        <ListLink />
-        <PrimaryButton variant='outlined'>Đăng nhập</PrimaryButton>
-        <PrimaryButton>Đăng ký</PrimaryButton>
+        <div
+          className='container flex w-full flex-col gap-2 bg-white py-4 shadow-sm'
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+        >
+          <ListLink />
+          <PrimaryButton variant='outlined'>Đăng nhập</PrimaryButton>
+          <PrimaryButton>Đăng ký</PrimaryButton>
+        </div>
       </div>
     </>
   )
