5744643e2ee71c73072495fc9344ef1bb1f6d155
nobi-onway <doangiabao.dev@gmail.com>
2024-07-04 17:37:03 +0700

    fix: base carousel cannot work as what i want to

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -4,10 +4,16 @@ import WhatFor from '@/components/Business/WhatFor'
 import WhyFor from '@/components/Business/WhyFor'
 import BaseCarousel from '@/components/Common/Carousel/BaseCarousel'
 
+const banner_images = [
+  'https://business.courtsite.my/images/custom_pricing.svg',
+  'https://business.courtsite.my/images/pricing_plans_v2.png',
+  'https://business.courtsite.my/images/cover.jpg',
+]
+
 const Business = () => {
   return (
     <div>
-      <BaseCarousel />
+      <BaseCarousel images={banner_images} />
       <WhatFor />
       <WhyFor />
     </div>
