1924fe660c2b84457c02a8921c84ed058ce6069c
tgound1403 <nguyentrieuduong14032002@gmail.com>
2024-07-09 16:14:15 +0700

    fix: introduction section not responsive on mobile size

--- src/components/Home/Introduction/index.tsx
+++ src/components/Home/Introduction/index.tsx
@@ -10,14 +10,16 @@ export const Introduction = () => {
   ]
 
   return (
-    <section className='w-full bg-white'>
-      <div className='flex flex-row items-start'>
+    <section className='bg-white'>
+      <div className='flex flex-col items-center justify-center lg:flex-row lg:items-start'>
         {/* Call To Action group */}
-        <div className='sticky top-[165px] flex min-h-[80vh] flex-1 flex-col items-center justify-center'>
+        <div className='top-[165px] flex min-h-[40vh] flex-col items-center justify-center lg:sticky lg:min-h-[80vh] lg:flex-1'>
           <div className=''>
-            <p className='text-[64px] font-bold'>Hệ thống sân đa dạng</p>
-            <p className='mt-5 text-xl font-bold text-primary'>Đa dạng sự lựa chọn dựa trên vị trí của bạn</p>
-            <PrimaryButton radius='full' className='mt-11 w-fit !py-3 pl-8 pr-6'>
+            <p className='text-[36px] font-bold lg:text-[64px]'>Hệ thống sân đa dạng</p>
+            <p className='mt-5 text-[14px] font-bold text-primary lg:text-xl'>
+              Đa dạng sự lựa chọn dựa trên vị trí của bạn
+            </p>
+            <PrimaryButton radius='full' className='mt-[20px] w-fit !py-3 pl-8 pr-6 lg:mt-11'>
               <span className='flex items-center gap-2 text-primary'>
                 <span className='text-sm font-bold sm:text-base'>Xem thêm</span>
                 <img className='h-8 w-8' src='/images/svg/leftArrow.svg' alt='left arrow' />
@@ -26,19 +28,15 @@ export const Introduction = () => {
           </div>
         </div>
         {/* Images Introduction */}
-        <div>
-          {[
-            new Array(3)
-              .fill(1)
-              .map((_, index) => (
-                <img
-                  key={index}
-                  className='my-1 h-[88vh] w-[52vw] rounded-lg object-cover'
-                  src='/images/png/introduction1.png'
-                  alt='introduction 1'
-                />
-              )),
-          ]}
+        <div className='flex flex-row flex-nowrap overflow-scroll lg:h-auto lg:flex-1 lg:flex-col'>
+          {introductionImages.map((item, index) => (
+            <img
+              key={index}
+              className='mr-[8px] h-[100vw] w-[100vw] w-screen object-cover lg:mb-[8px] lg:h-[88vh] lg:w-[52vw] lg:rounded-lg'
+              src={item}
+              alt='introduction 1'
+            />
+          ))}
         </div>
       </div>
     </section>
