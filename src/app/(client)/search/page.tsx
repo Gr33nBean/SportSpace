662930221c391adcb98e6a7ecbbcdd276306430f
viet.dang <viet.dang@kegmil.com>
2024-07-14 00:22:38 +0700

    fix: handle put component inside search page

--- src/app/(client)/search/page.tsx
+++ src/app/(client)/search/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import FilterCourt from '@/components/_shared/FilterCourt'
+import CourtList from '@/components/Search/CourtList'
 
 const Search = () => {
-  return <div>Search</div>
+  return (
+    <main className='container mt-10 flex flex-col gap-8 md:flex-row'>
+      <FilterCourt />
+      <CourtList />
+    </main>
+  )
 }
 
 export default Search
--- src/components/Common/BottomSheet/index.tsx
+++ src/components/Common/BottomSheet/index.tsx
@@ -1,10 +1,14 @@
+'use client'
 import React from 'react'
 import clsx from 'clsx'
+
+import 'react-spring-bottom-sheet/dist/style.css'
 
 type BottomSheetProps = {
   children: React.ReactNode
   open: boolean
   onClose?: () => void
+  onDismiss?: () => void
   className?: string
 }
 
