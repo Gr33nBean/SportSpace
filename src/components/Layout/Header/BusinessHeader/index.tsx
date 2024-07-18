ad59adfe6a96768213b7b86d2448dbdbf878d659
Gr33nBean <20520206@gm.uit.edu.vn>
2024-07-18 23:21:12 +0700

    feat: business header with navbar

--- src/components/Layout/Header/BusinessHeader/index.tsx
+++ src/components/Layout/Header/BusinessHeader/index.tsx
@@ -1,15 +1,27 @@
 'use client'
 import React, { useState } from 'react'
+import { House, Mail, Users } from 'lucide-react'
 
 import { routes } from '@/config/routes'
 
-import BaseHeader from '../BaseHeader'
+import BaseHeaderBusiness from '../BaseHeaderBusiness'
 import ListLink, { PageType } from '../ListLink'
 
 const pages: PageType[] = [
   {
     name: 'Doanh nghiệp',
     href: routes.business,
+    icon: <House />,
+  },
+  {
+    name: 'About Us',
+    href: routes.aboutUs,
+    icon: <Users />,
+  },
+  {
+    name: 'Liên lạc',
+    href: routes.contact,
+    icon: <Mail />,
   },
 ]
 
@@ -17,11 +29,9 @@ const BusinessHeader = () => {
   const [expand, setExpand] = useState(false)
   return (
     <>
-      <BaseHeader hambugerProps={{ expand, setExpand }} rightContent={<></>}>
-        <div className='flex w-full flex-col justify-center lg:flex-row lg:justify-start lg:px-12'>
-          <ListLink pages={pages} />
-        </div>
-      </BaseHeader>
+      <BaseHeaderBusiness hambugerProps={{ expand, setExpand }} rightContent={<></>}>
+        <ListLink pages={pages} />
+      </BaseHeaderBusiness>
     </>
   )
 }
--- src/components/Layout/Header/ListLink.tsx
+++ src/components/Layout/Header/ListLink.tsx
@@ -7,21 +7,31 @@ import React from 'react'
 export type PageType = {
   name: string
   href: string
+  icon?: React.ReactNode
 }
 
 const ListLink = ({ pages }: { pages: PageType[] }) => {
   const path = usePathname()
+
   return (
     <>
-      {pages.map((page) => (
-        <Link
-          key={page.name}
-          href={page.href}
-          className={`block w-full text-center text-sm font-medium text-primary transition-all duration-150 hover:underline md:w-fit ${'/' + path.split('/')[1] === page.href ? '!font-bold' : ''}`}
-        >
-          {page.name}
-        </Link>
-      ))}
+      {pages.map((page) => {
+        const isActive = '/' + path.split('/')[1] === page.href
+        return (
+          <Link
+            key={page.name}
+            href={page.href}
+            className={`block w-full text-center text-sm font-medium text-primary transition-all duration-150 hover:underline max-sm:flex-1 md:w-fit ${isActive ? '!font-bold' : ''}`}
+          >
+            <span className='max-sm:hidden'>{page.name}</span>
+            <span className='flex w-full items-center justify-center sm:hidden'>
+              <span className={`rounded-md p-2 ${isActive ? 'bg-primary text-white' : 'text-custom-gray'}`}>
+                {page.icon}
+              </span>
+            </span>
+          </Link>
+        )
+      })}
     </>
   )
 }
