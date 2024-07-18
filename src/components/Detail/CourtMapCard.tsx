6bef8d1df7946e273fb292c6f5b1ce6df3edad20
viet.dang <viet.dang@kegmil.com>
2024-07-18 13:49:36 +0700

    feat: handle create a link to gg map

--- src/components/Detail/CourtMapCard.tsx
+++ src/components/Detail/CourtMapCard.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
+import LinkGGMap from '../_shared/LinkGGMap'
 import StaticMap from '../Common/Map/StaticMap'
 
-const GG_MAP_URL = 'https://www.google.com/maps/place'
 const address = '263/3 đường Trần Não, Phường Bình An, Quận 2, thành phố Hồ Chí Minh'
-const encodedAddress = encodeURIComponent(address)
 
 const CourtMapCard = () => {
   return (
-    <a target='_blank' href={`${GG_MAP_URL}/${encodedAddress}`} rel='noopener noreferrer'>
+    <LinkGGMap link={address}>
       <div className='relative m-4 h-[15rem] w-[25rem] overflow-hidden rounded-xl hover:cursor-pointer'>
         <StaticMap />
         <div className='absolute bottom-0 left-0 z-10 flex w-full max-w-[20rem] flex-col gap-1 px-4 py-5'>
@@ -18,7 +17,7 @@ const CourtMapCard = () => {
           </div>
         </div>
       </div>
-    </a>
+    </LinkGGMap>
   )
 }
 
