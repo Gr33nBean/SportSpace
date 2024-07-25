7e9141e1e6e207d029d10f3761cd964908dfd2ba
TrungTinns <trungtin073@gmail.com>
2024-07-25 12:40:00 +0700

    feat: sign in api

--- src/components/Auth/SignInModalForm.tsx
+++ src/components/Auth/SignInModalForm.tsx
@@ -1,17 +1,47 @@
 import { FieldValues } from 'react-hook-form'
 
+import { signIn } from '@/config/api/auth'
+import { routes } from '@/config/routes'
+import { usePost } from '@/hooks/api-hooks'
+
 import BaseAuthModalForm from './BaseAuthModalForm'
+import NavigateToBusiness from './NavigateToBusiness'
 import SignInForm from './SignInForm'
+import SuccessNotification from './SuccessNotification'
+
+export type OtpConfirmationFormFields = {
+  email: string
+  password: string
+}
 
 const SignInModalForm = ({ open = false, handleClose }: { open?: boolean; handleClose: () => void }) => {
+  const { mutate, isSuccess } = usePost(
+    signIn(),
+    {},
+    (data) => {
+      console.log('Sign in successful:', data)
+    },
+    (error) => {
+      console.error('Sign in failed:', error)
+    }
+  )
+
   const onSubmit = (data: FieldValues) => {
     console.log(data)
+    mutate(data)
   }
 
   return (
     <>
       <BaseAuthModalForm open={open} handleClose={handleClose} callback={onSubmit} title='Tài khoản'>
-        <SignInForm callback={onSubmit} />
+        {isSuccess ? (
+          <div className='flex flex-col gap-8'>
+            <SuccessNotification buttonTitle='Đến trang chủ' message='Đăng nhập thành công' route={routes.home} />
+            <NavigateToBusiness />
+          </div>
+        ) : (
+          <SignInForm callback={onSubmit} />
+        )}
       </BaseAuthModalForm>
     </>
   )
