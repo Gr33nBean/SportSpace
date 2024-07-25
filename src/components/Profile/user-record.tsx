ac0d1322f61485eedcb1450934c3f465873c8a75
TrungTinns <trungtin073@gmail.com>
2024-07-26 03:03:17 +0700

    feat: sign in api(update)

--- src/components/Profile/user-record.tsx
+++ src/components/Profile/user-record.tsx
@@ -3,22 +3,37 @@ import React from 'react'
 type UserRecordProps = {
   userName: string
   enrollDate: Date
+  count: number
+  timeBooked: number
 }
 
 const UserRecord = (props: UserRecordProps) => {
-  const { userName, enrollDate } = props
+  const { userName, enrollDate, count, timeBooked } = props
   const month = enrollDate.toLocaleString('vi-VN', { month: 'long' })
   const year = enrollDate.getFullYear()
   return (
-    <div className='flex items-center p-4'>
-      <img className='h-12 w-12 rounded-full' src='https://placehold.co/48x48' alt='User profile picture' />
-      <div className='ml-4'>
-        <h2 className='text-lg font-semibold text-zinc-900'>{`${userName}`}</h2>
-        <p className='text-zinc-500'>
-          Tham gia từ tháng {`${month}`}, {`${year}`}
-        </p>
+    <>
+      <div className='flex items-center p-4'>
+        <img className='h-12 w-12 rounded-full' src='https://placehold.co/48x48' alt='User profile picture' />
+        <div className='ml-4'>
+          <h2 className='text-lg font-semibold text-black'>{`${userName}`}</h2>
+          <p className='text-zinc-500'>
+            Tham gia từ tháng {`${month}`}, {`${year}`}
+          </p>
+        </div>
       </div>
-    </div>
+      <div className='flex justify-around bg-secondary p-4'>
+        <div className='text-center'>
+          <p className='text-3xl font-bold text-black'>{`${count}`}</p>
+          <p className='text-zinc-700'>Số lần đặt</p>
+        </div>
+        <div className='border-l border-zinc-400'></div>
+        <div className='text-center'>
+          <p className='text-3xl font-bold text-black'>{`${timeBooked}`}</p>
+          <p className='text-zinc-700'>Số giờ đặt</p>
+        </div>
+      </div>
+    </>
   )
 }
 
